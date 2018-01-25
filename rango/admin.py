0cad7e47c001249657807996fd3840df7426377a
Sam <2254258H@student.gla.ac.uk>
2018-01-25 15:53:18 +0000

    End of Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -3,5 +3,16 @@ from rango.models import Category, Page
 
 # Register your models here.
 
+
+
+class PageAdmin(admin.ModelAdmin):
+    #fields = ['title','category','url']
+    #fieldsets = [(None, {'fields':['title']}),('Category',{'fields':['category']}),]
+    #model = Page
+    list_display = ('title','category','url')
+    #def get_name(self):
+    #    return list_display
+
+    
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page,PageAdmin)
